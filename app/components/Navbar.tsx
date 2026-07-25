799d242dbcbb339d229bbd30980292a2bbe7b531
Rahmannn <saifulloh.rahman028@student.unud.ac.id>
2026-07-25 22:59:13 +0700

    update logo

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -158,7 +158,7 @@ export default function Navbar() {
                 "
             >
                 <Image
-                    src="/logo-rahman-v2.png"
+                    src="/rahman-new.png"
                     alt="Saifulloh Rahman logo"
                     width={26}
                     height={60}
